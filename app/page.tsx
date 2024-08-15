2dffcea1f76280b894bee622a591bea5cfeec820
NextSap <nextsap@outlook.com>
2024-08-15 10:57:04 +0200

    update version name

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="flex flex-col gap-10 md:w-[50%] w-[90%] m-auto mt-5">
-            <div className="flex justify-center items-center w-full">
+            <div className="flex flex-col justify-center items-center w-full">
                 <Image src="/IHF Logo.png" width={100} height={100} alt="IHF Logo"/>
+                <p>May 2024 version</p>
             </div>
             <TestCard/>
             <RulesCard/>
