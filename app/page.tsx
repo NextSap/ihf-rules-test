01b5404d87b520d699dd19afb28eedadec885ad3
NextSap <nextsap@outlook.com>
2025-07-08 23:28:21 +0200

    update july 2025 version

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="flex flex-col gap-10 md:w-[50%] w-[90%] m-auto mt-5">
             <div className="flex flex-col justify-center items-center w-full">
                 <Image src="/IHF Logo.png" width={100} height={100} alt="IHF Logo"/>
-                <p>May 2024 version</p>
+                <p>July 2025 version</p>
             </div>
             <TestCard/>
             <RulesCard/>
